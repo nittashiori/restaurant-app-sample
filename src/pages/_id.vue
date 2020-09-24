05a6d0cca9a1b521380c721850fe11676d606b77
nittashiori <nitta@impv.co.jp>
2020-09-24 15:31:54 +0900

    詳細ページへの遷移、お店のID表示

--- src/pages/_id.vue
+++ src/pages/_id.vue
@@ -3,3 +3,38 @@
     {{ $route.params.id }}
   </div>
 </template>
+
+<script>
+export default {
+  async asyncData({ $axios, params, env, redirect }) {
+    try {
+      // データ取得
+      const { data } = await $axios.get(
+        'http://localhost:3000/api/gourmet/v1/',
+        {
+          params: {
+            key: process.env.apikey,
+            id: params.id,
+            format: 'json',
+          },
+        }
+      )
+      // 対象データがない場合は一覧ページに戻る
+      if (data.results.shop.length <= 0) {
+        redirect('/')
+      }
+      return {
+        shop: data.results.shop[0],
+        error: undefined,
+      }
+    } catch (error) {
+      return {
+        shop: undefined,
+        error: {
+          message: 'データの取得に失敗しました',
+        },
+      }
+    }
+  },
+}
+</script>
