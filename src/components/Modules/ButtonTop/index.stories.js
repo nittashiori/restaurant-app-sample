83634287082d10bec50e611cc22c0171a572016e
nittashiori <nitta@impv.co.jp>
2020-10-27 13:31:06 +0900

    ButtonTopコンポーネント追加

---
+++ src/components/Modules/ButtonTop/index.stories.js
@@ -0,0 +1,33 @@
+import ButtonTop from './index.vue'
+
+export default {
+  title: 'Modules/ButtonTop',
+  component: ButtonTop,
+  argTypes: {
+    color: {
+      control: { type: 'select', options: ['default', 'white', 'primary'] },
+    },
+    onClick: { action: 'clicked' },
+  },
+}
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { ButtonTop },
+  template: '<ButtonTop v-bind="$props" @onClick="onClick" />',
+})
+
+export const Default = Template.bind({})
+Default.args = {
+  color: 'default',
+}
+
+export const White = Template.bind({})
+White.args = {
+  color: 'white',
+}
+
+export const Primary = Template.bind({})
+Primary.args = {
+  color: 'primary',
+}
---
+++ src/components/Modules/ButtonTop/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <a :class="classes" @click="onClick">
+    <IconTop :color="color" />
+    <div class="button-top__text">上部に戻る</div>
+  </a>
+</template>
+
+<script>
+import IconTop from '~/components/Atoms/IconTop'
+
+export default {
+  components: {
+    IconTop,
+  },
+  props: {
+    color: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    classes() {
+      return {
+        [`button-top`]: true,
+        [`button-top--${this.color}`]: true,
+      }
+    },
+  },
+  methods: {
+    onClick() {
+      this.$emit('onClick')
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.button-top {
+  display: block;
+  width: 80px;
+  cursor: pointer;
+  &--default {
+    color: $text-color;
+  }
+  &--white {
+    color: $white-color;
+  }
+  &--primary {
+    color: $primary-color;
+  }
+  .icon-top {
+    margin: auto;
+  }
+  &__text {
+    margin: 8px 0 0;
+    @include font-bold;
+    @include fts(8.75);
+    text-align: center;
+  }
+}
+</style>
