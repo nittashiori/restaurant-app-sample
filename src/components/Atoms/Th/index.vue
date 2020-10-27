2a8703fbae876742068d942a5f667400e1a292f7
nittashiori <nitta@impv.co.jp>
2020-10-27 18:49:28 +0900

    th,tdコンポーネントのスタイル調整

--- src/components/Atoms/Th/index.vue
+++ src/components/Atoms/Th/index.vue
@@ -4,9 +4,15 @@
 
 <style lang="scss" scoped>
 .th {
+  width: 100%;
   max-width: 160px;
   padding: 24px 16px;
   border-top: 1px solid $primary-color;
   @include font-bold;
+  @include media(md, max) {
+    max-width: 80px;
+    padding: 8px;
+    @include fts(8.75);
+  }
 }
 </style>
