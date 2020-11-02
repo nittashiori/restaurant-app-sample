7f94e007c08be888694ba5fadde404137956ef41
nittashiori <nitta@impv.co.jp>
2020-11-02 16:34:11 +0900

    クリック領域を広げた

--- src/components/Modules/ButtonTop/index.vue
+++ src/components/Modules/ButtonTop/index.vue
@@ -1,7 +1,9 @@
 <template>
   <a :class="classes" @click="onClick">
-    <IconTop :color="color" />
-    <div class="button-top__text">上部に戻る</div>
+    <div class="button-top__inner">
+      <IconTop :color="color" />
+      <div class="button-top__text">上部に戻る</div>
+    </div>
   </a>
 </template>
 
@@ -36,17 +38,28 @@ export default {
 
 <style lang="scss" scoped>
 .button-top {
-  display: block;
-  width: 80px;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 40px 0;
+  transition: all 0.3s ease;
   cursor: pointer;
   &--default {
     color: $text-color;
+    background: $white-color;
+    &:hover {
+      background: darken($white-color, 5%);
+    }
   }
   &--white {
     color: $white-color;
+    background: $primary-color;
+    &:hover {
+      background: darken($primary-color, 5%);
+    }
   }
-  &--primary {
-    color: $primary-color;
+  &__inner {
+    width: 80px;
   }
   .icon-top {
     margin: auto;
