337528ced29f91838c59e91a9f8b4873e230bf0e
nittashiori <nitta@impv.co.jp>
2020-11-19 16:32:22 +0900

    環境ごとにenvを取得する

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-require('dotenv').config()
-
 export default {
   /*
    ** Nuxt rendering mode
@@ -57,9 +55,17 @@ export default {
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     '@nuxtjs/style-resources',
-    '@nuxtjs/dotenv',
     'nuxt-webfontloader',
     'nuxt-clipboard2',
+    [
+      '@nuxtjs/dotenv',
+      {
+        filename:
+          process.env.NODE_ENV !== 'production'
+            ? './config/.env.dev'
+            : './config/.env.prod',
+      },
+    ],
   ],
   webfontloader: {
     google: {
